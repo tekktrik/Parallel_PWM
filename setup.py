387cd52c59240c4b1ff46802a742ae5a4d5b8d32
tekktrik <89490472+tekktrik@users.noreply.github.com>
2021-08-31 22:34:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from Cython.Build import cythonize
 setup(
     name='Cython PWM function',
     ext_modules=cythonize([
-        "bitbang_pwm.py"
+        "bitbang_pwm.py",
+        "bitbang_i2c.py"
     ], annotate=True, compiler_directives={'language_level' : "3"}),
     zip_safe=False,
 )
